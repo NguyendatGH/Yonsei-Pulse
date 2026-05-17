2777f05a8d9e6bd8ff9ace4be315e7b4e2f8ea61
NguyendatGH <tandatbtqb@gmail.com>
2026-05-17 09:52:44 +0700

    fix/ fix dark theme error

--- hooks/use-theme.ts
+++ hooks/use-theme.ts
@@ -1,16 +1,10 @@
-import { useColorScheme } from 'react-native';
-import { Colors, DarkColors } from '../constants/theme';
+import { Colors } from '../constants/theme';
 
 export function useTheme() {
-  const colorScheme = useColorScheme();
-  
-  // You can extend this to use a manual setting from a context/store
-  const isDark = colorScheme === 'dark';
-  const theme = isDark ? DarkColors : Colors;
-
   return {
-    theme,
-    isDark,
-    colorScheme,
+    theme: Colors,
+    isDark: false,
+    colorScheme: 'light' as const,
+    toggleTheme: () => {},
   };
 }
